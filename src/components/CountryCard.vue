ee242a14e74ae06e87d8fc637deeb7761acad0b5
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-05-28 10:24:42 +0300

    create country card component for the home page

---
+++ src/components/CountryCard.vue
@@ -0,0 +1,37 @@
+<script setup>
+const props = defineProps({
+    name: String,
+    emoji: String
+})
+</script>
+
+<template>
+    <div class="card-wrapper bg-[#F0E9E9] rounded-2xl flex flex-col space-y-4 mb-5">
+      <div class="flex h-3/4 rounded-2xl justify-center items-center">
+        <div class="flag">
+          {{ emoji }}
+        </div>
+      </div>
+      <div class="flex bg-[#6770C3] h-1/4 rounded-2xl text-white justify-center items-center">
+        <div class="text-2xl text-center">
+          {{ name }}
+        </div>
+      </div>
+    </div>
+</template>
+
+<style scoped>
+.card-wrapper {
+    height: 230px;
+    width: 210px;
+    cursor: pointer;
+}
+
+.flag {
+  font-size: 210px;
+}
+
+span {
+  font-family: 'Roboto', sans-serif;
+}
+</style>
