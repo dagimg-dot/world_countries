c71a59f45b5462e8d8d84dcb8b224acdba7a5d48
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-05-28 11:47:41 +0300

    use header in both pages

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,22 @@
 <script setup>
+import { computed } from 'vue'
+
+const props = defineProps({
+    title: String
+})
+
+const firstWord = computed(() => props.title.slice(0, props.title.indexOf(' ')))
+const secondWord = computed(() => props.title.slice(props.title.indexOf(' ')))
 
 </script>
 <template>
-    <div class="flex bg-[#6770C3] h-24 justify-center text-4xl text-white font-serif"> 
-        <div class="title flex flex-col justify-center items-center">
-            <div>World</div>
-            <div class="-mt-2">Countries</div>
+    <div class="flex bg-[#6770C3] h-24 justify-center text-4xl text-white font-serif">
+        <div v-if="props.title == 'World Countries'" class="title flex flex-col justify-center items-center">
+            <div>{{ firstWord }}</div>
+            <div class="-mt-2">{{ secondWord }}</div>
+        </div>
+        <div v-else class="title flex flex-col justify-center items-center">
+            {{ title }}
         </div>
     </div>
 </template>
