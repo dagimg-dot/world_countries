01828cf27087db3a18e8dbda3e70419078644391
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-05-28 10:25:03 +0300

    create header and main component for the home page

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+<template>
+    <div class="flex bg-[#6770C3] h-24 justify-center text-4xl text-white font-serif"> 
+        <div class="title flex flex-col justify-center items-center">
+            <div>World</div>
+            <div class="-mt-2">Countries</div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.title {
+    font-family: 'Roboto', sans-serif;
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,61 @@
+<script setup>
+import { onMounted } from 'vue'
+import CountryCard from '../components/CountryCard.vue'
+import { apolloClient } from '../apollo' 
+import gql from 'graphql-tag'
+import { ref } from 'vue'
+import router from '../router/router'
+
+const fetchCountries = async () => {
+  try {
+    const { data } = await apolloClient.query({
+      query: gql`
+        query getCountries {
+          countries {
+            name
+            code
+            emoji
+          }
+        }
+      `,
+    })
+
+    return data.countries
+  } catch (error) {
+    console.error('Error while fetching countries:', error)
+    throw error
+  }
+}
+
+const countries = ref([])
+const clickedCountry = ref('')
+
+const handleClick = () => {
+  router.push({ name: 'details', params: { id: clickedCountry.value } })
+}
+
+const isLoading = ref(true)
+
+onMounted(async () => {
+  try {
+    countries.value = await fetchCountries()
+  } finally {
+    isLoading.value = false
+  }
+})
+</script>
+
+<template>
+  <div class="main bg-[#F0E9E9] flex">
+    <div class="flex flex-wrap ">
+      <!-- render this div after the data has arrived -->
+
+      <div v-if="!isLoading" v-for="country in countries" :key="country.name" class="w-1/3">
+        <CountryCard :name="country.name" :emoji="country.emoji" @click="clickedCountry = country.code; handleClick()" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
+
